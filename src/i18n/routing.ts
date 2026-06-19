22dd76f34d2446ccd6e5e24b92c8926effaedf34
Anton <bogachuk566@gmail.com>
2026-06-19 08:55:27 +0300

    ашч: disable alternateLinks in internationalization routing configuration

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { locales, DEFAULT_LOCALE } from '@/config/locales';
 export const routing = defineRouting({
   locales,
   defaultLocale: DEFAULT_LOCALE,
+  alternateLinks: false,
 });
 
 export type Locale = (typeof routing.locales)[number];
